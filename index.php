b985d40d54b03aa98b472871a974a21133b5580e
JESSICA MAIA <JESSICA.MAIA@DIGITALCOLLEGE.LOCAL>
2024-03-09 11:07:08 -0300

    logout.php

--- index.php
+++ index.php
@@ -14,6 +14,28 @@ $client->addScope('profile');
 
 if(!isset($_SESSION['access_token'])) {
     $auth_url = $client->createAuthUrl();
-    header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL))
+    header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
 }
+
+if (isset($_GET['code'])){
+    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    $client->setAccessToken($token);
+
+    $_SESSION['access_token'] = $token;
+}
+
+if ($client->getAccesToken()){
+    $oauth2 = new Google\Service\Oauth2($client);
+    $user_info = $oauth2->userinfo->get();
+
+    echo 'Bem vindo, ' . htmlspecialchars($user_info->name) . '!';
+
+    echo '<img src="' . htmlspecialchars($user_info->picture) . '" alt="Imagem do perfil">';
+
+    echo '<br><a href="logout.php">Sair</a>';
+}else{
+    echo 'Erro de autenticação.';
+}
+
+
 ?>
